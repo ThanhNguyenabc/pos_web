c69b131bb81ca16cc222814e718383195cfa4c11
Thanh Nguyen <thanhnguyen95abc@gmail.com>
2022-12-14 18:01:41 +0700

    add base button styles

---
+++ front-end/utils/ColorUtil.js
@@ -0,0 +1,17 @@
+module.exports = {
+  primary: "#FF5A22",
+  secondary: "#540375",
+  accent: "#FFD8C6",
+  blue_light: "#D7D9FF",
+  purple_light: "#E7DBEB",
+  info: "#0B70FE",
+  warning: "#F79009",
+  success: "#039855",
+  error: "#D92D20",
+  neutral_100: "#F2F4F7",
+  neutral_300: "#D0D5DD",
+  neutral_400: "#98A2B3",
+  neutral_600: "#475467",
+  netral_700: "#344054",
+  netral_900: "#101828",
+};
