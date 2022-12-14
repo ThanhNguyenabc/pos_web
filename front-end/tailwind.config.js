2a4e17d174ffd0dfcb0efc2df513cab491dbf8eb
Thanh Nguyen <thanhnguyen95abc@gmail.com>
2022-12-14 16:46:14 +0700

    config base color and typo

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,19 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+// import Colors from "./utils/ColorUtil.js";
+// const defaultTheme = require("tailwindcss/defaultTheme");
+
+const Colors = require("./utils/ColorUtil.js");
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+
+          ...Colors,
+          "::placeholder": {
+            color: Colors.neutral_400,
+          },
+          "::-ms-input-placeholder": {
+            color: Colors.neutral_400,
+          },
+        },
+      },
+    ],
+  },
   theme: {
     extend: {
       colors: {
-        main: "#080A1A",
-        subMain: "#F20000",
-        dry: "#0B0F29",
-        star: "#FFB000",
-        text: "",
+        current: Colors.warning,
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
       },
     },
   },
-  plugins: [require("daisyui")],
 };
