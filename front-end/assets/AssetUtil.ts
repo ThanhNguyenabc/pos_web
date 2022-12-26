c716881d550dc24222d19e3bd423c1441217f4c0
Thanh Nguyen <thanhnguyen95abc@gmail.com>
2022-12-26 10:48:43 +0700

    add resources

--- front-end/assets/AssetUtil.ts
+++ front-end/assets/AssetUtil.ts
@@ -1,1 +1,31 @@
+// export images
 export { default as Logo } from "./images/logo.jpg";
+export { default as FooterLogo } from "./images/bestpos.png";
+export { default as BannerImage } from "./images/pos.png";
+export { default as BusinessCategory } from "./images/business_img.png";
+export { default as QuickServiceCategory } from "./images/quickservice_img.png";
+export { default as RetailCategory } from "./images/retail_img.png";
+export { default as PizzeriaCategory } from "./images/pizzeria_img.png";
+export { default as RestaurantCategory } from "./images/restaurant_img.png";
+export { default as BarCategory } from "./images/bar_img.png";
+export { default as BusinessReviewImg } from "./images/business_review.png";
+export { default as ScheduleImg } from "./images/schedule.png";
+export { default as BrainStormImg } from "./images/brain_storm.png";
+export { default as POS2Img } from "./images/pos_2.png";
+export { default as PricingImg } from "./images/pricing.png";
+export { default as EasyUsingImg } from "./images/easy_use.png";
+export { default as HardwareImg } from "./images/hardware.png";
+export { default as SupportImg } from "./images/support.png";
+export { default as DashboardImg } from "./images/dashboard.png";
+
+// export icons
+export { default as IcMenu } from "./icons/ic_menu.svg";
+export { default as IcMobileLogo } from "./icons/ic_mobile_logo.svg";
+export { default as IcPhone } from "./icons/ic_phone.svg";
+export { default as IcFb } from "./icons/ic_fb.svg";
+export { default as IcLinkedLn } from "./icons/ic_linkedln.svg";
+export { default as IcInsta } from "./icons/ic_insta.svg";
+export { default as IcRightArrow } from "./icons/ic_right_arrow.svg";
+export { default as IcCheck } from "./icons/ic_check.svg";
+export { default as IcClose } from "./icons/ic_close.svg";
+export { default as IcChervonRight } from "./icons/ic_chervon_right.svg";
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,12 +2,37 @@
 // import Colors from "./utils/ColorUtil.js";
 // const defaultTheme = require("tailwindcss/defaultTheme");
 
-const Colors = require("./utils/ColorUtil.js");
+const Colors = require("./utils/ColorUtils.js");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
+  theme: {
+    container: {
+      screens: {
+        "2xl": "1440px",
+      },
+    },
+    extend: {
+      colors: {
+        neutral: {
+          100: "#F2F4F7",
+          300: "#D0D5DD",
+          400: "#98A2B3",
+          600: "#475467",
+          700: "#344054",
+          900: "#101828",
+          dark: "#020202",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        body: ['"Inter"'],
+      },
+    },
+  },
   plugins: [require("daisyui")],
   daisyui: {
     themes: [
@@ -25,15 +50,5 @@ module.exports = {
         },
       },
     ],
-  },
-  theme: {
-    extend: {
-      colors: {
-        current: Colors.warning,
-      },
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
-      },
-    },
   },
 };
