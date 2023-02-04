b661de12f77d69897a38a37e7d484f3d0b3240a6
ThanhNguyen <thanhnguyen95abc@gmail.com>
2023-02-04 14:40:27 +0700

    update common question

--- front-end/components/elements/breadme/CommonQuestion.tsx
+++ front-end/components/elements/breadme/CommonQuestion.tsx
@@ -6,36 +6,27 @@ import IcPLus from "assets/icons/ic_plus.svg";
 
 const CommonQuestionData = [
   {
-    title: "Is there a free trial available?",
+    title: "What is the cash discount program?",
     answer:
-      "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+      "The cash discount program allows you as a merchant to offset most of your merchant service fees. It is a method of implementing a service fee to all customers, while providing a discount to those who pay with cash.",
   },
   {
-    title: "Can I change my plan later?",
+    title: "Is it legal to offer a cash discount?",
     answer:
-      "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+      "Yes, cash discount is legal. Law prohibits payment card network such as Visa from inhibiting the ability to provide a discount for payment by cash, checks, debit cards or credit cards.",
   },
   {
-    title: "What is your cancellation policy?",
+    title: "What if I don't like the cash discount program?",
     answer:
-      "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+      "If you are not a fan of the cash discount program, we have a 100% satisfaction guarantee. If for any reason you want to stop using our cash discount program, we will immediately switch you back to the traditional pricing.",
   },
   {
-    title: "Can other info be added to an invoice?",
+    title: "Is cash discount the same as a surcharge?",
     answer:
-      "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
-  },
-  {
-    title: "How does billing work?",
-    answer:
-      "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
-  },
-  {
-    title: "How do I change my account email?",
-    answer:
-      "Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free, personalized 30-minute onboarding call to get you up and running as soon as possible.",
+      "No, it is not. A surcharge is a fee added to a credit card transaction to cover the credit card processing costs. With cash discount, a discount is applied to the total amount when a customer pays with cash.",
   },
 ];
+
 const CommonQuestion = () => {
   return (
     <div className="flex flex-col px-4 py-12 gap-10 md:px-8 mx-auto max-w-[768px] lg:py-[100px] xl:py-[120px]">
