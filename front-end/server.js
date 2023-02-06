2b0db4ae2d87b4cfdcd1172c76c7ba6690590a23
ThanhNguyen <thanhnguyen95abc@gmail.com>
2023-02-06 17:27:06 +0700

    update server js file

--- front-end/server.js
+++ front-end/server.js
@@ -3,7 +3,7 @@ const {parse} = require("url");
 const next = require("next");
 const dev = process.env.NODE_ENV !== "production";
 const hostname = process.env.HOST_NAME || "localhost";
-const port = process.env.PORT || "3000";
+const port = process.env.PORT || 3000;
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
