7505ebbdbfd560976dd1caa950a01f3dac03350c
ThanhNguyen <thanhnguyen95abc@gmail.com>
2023-02-08 16:40:01 +0700

    add custom server js file

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -4,10 +4,15 @@ import { Layout } from "components/common/Layout";
 import ThankYouDialog from "components/common/ThankYouDialog";
 import RequestDemoPOS from "components/elements/request_demo_pos/RequestDemoPOS";
 import FindPOSModal from "components/elements/find_pos_modal/FindPOSModal";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Best pos</title>
+        <meta name="best pos" />
+      </Head>
       <ThankYouDialog />
       <RequestDemoPOS />
       <FindPOSModal />
