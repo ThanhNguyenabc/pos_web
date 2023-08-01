1d92ee7ad1863954ece8b85d85fa022aac8cf0f8
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-08-01 18:16:18 +0700

    remove files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,58 @@
-import IcCheck from "assets/icons/ic_check.svg";
 import IcRightArrow from "assets/icons/ic_right_arrow.svg";
 import HelpingSection from "components/elements/home/HelpingSection";
-import ClientSection from "components/common/ClientSection";
-import BusinessCategorySection from "components/elements/home/BusinessCategorySection";
 import { Button } from "components/common/Button";
 import MetricSection from "components/common/MetricSection";
 import { FooterCTA } from "components/common/FooterCTA";
 import { useRouter } from "next/router";
-import ColorUtils from "utils/ColorUtils";
 import AppRoutes from "utils/routes";
-import HeroSection from "components/common/HeroSection";
-import TestimonialSection from "components/elements/home/TestimonialSection";
 import useTrans from "hooks/useTrans";
-import HTMLReactParser from "html-react-parser";
 import Image from "next/image";
+import Box from "components/common/Box";
+import BusinessCategorySection from "components/elements/home/BusinessCategorySection";
+import TestimonialSectionV2 from "components/elements/home/TestimonialSection";
 import HeadTag from "components/common/HeadTag";
 import { GetServerSidePropsContext } from "next";
 import { getListPOS } from "api_client/axios_client";
 import { Product } from "models/product.model";
+import { Locale } from "models/app_configs";
+import HTMLReactParser from "html-react-parser";
+import dayjs from "dayjs";
 
-const FeatureData = ["support", "free_trainning", "installment"];
+require("dayjs/locale/es");
+
+const Hp2Translation = {
+  heading: {
+    [Locale.en]:
+      "<span className='text-secondary'>Compare</span> & <span className='text-secondary'> Save</span> on a POS Today",
+    [Locale.es]:
+      "<span className='text-secondary'>Compare </span> y <span className='text-secondary'> ahorre </span> en un POS hoy",
+  },
+  title: {
+    [Locale.en]:
+      "We understand the overwhelming task of sifting through numerous Point of Sale systems available today. That's why we're on a mission to simplify things for you. Our goal is to help you select the perfect POS solution that suits your business like a glove. Check out our feedback.",
+    [Locale.es]:
+      "Entendemos la abrumadora tarea de revisar numerosos sistemas de Punto de Venta disponibles hoy en día. Por eso, estamos en una misión para simplificar las cosas para ti. Nuestro objetivo es ayudarte a seleccionar la solución perfecta de POS que se ajuste a tu negocio como un guante. Echa un vistazo a nuestros comentarios.",
+  },
+  description: {
+    [Locale.en]:
+      "Answer a few questions to find the POS that fits best for your business.",
+    [Locale.es]:
+      "Responde algunas preguntas para encontrar el POS que se adapte mejor a tu negocio.",
+  },
+  update_date: {
+    [Locale.en]: "Last updated: ",
+    [Locale.es]: "última vez actualizado: ",
+  },
+  start_now: {
+    [Locale.en]: "Start now",
+    [Locale.es]: "Empezar",
+  },
+};
 
 export const getServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
-  context.res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=43200, stale-while-revalidate=60"
-  );
-
   const products = await getListPOS({ type: "all", limit: 4 });
-
   return {
     props: { products },
   };
@@ -38,120 +60,59 @@ export const getServerSideProps = async (
 
 const HomePage = ({ products }: { products: Array<Product> }) => {
   const router = useRouter();
-  const { t } = useTrans();
+  const { t, locale } = useTrans();
 
   const findPOS = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
     router.push(AppRoutes.QuestionnairePage);
   };
 
-  const getPOSFree = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-    router.push(AppRoutes.FreePOSPage);
-  };
-
-  const FindPOSBtn = (
-    <Button
-      classname="md:h-16"
-      title={t("find_your_pos")}
-      style={{
-        background: ColorUtils["neutral-900"],
-      }}
-      onClick={findPOS}
-    />
-  );
-
-  const GetFreePOSBtn = (
-    <Button
-      classname="md:h-16"
-      title={t("get_free_pos")}
-      style={{ background: "white", color: "black" }}
-      onClick={getPOSFree}
-      rightIcon={<IcRightArrow className="text-lg" />}
-    />
-  );
-
   return (
     <>
       <HeadTag screen="home" />
-      <div className=" bg-gradient-to-b from-[#FF5A22] to-[#FFA722]">
-        <div className="flex flex-col max-w-[1320px] lg:h-[640px] lg:flex-row xl:ml-[calc((100%-1200px)/2)]">
-          <HeroSection className="flex-1 gap-4 text-center md:gap-6 lg:py-[80px] xl:py-[120px] lg:text-left my-auto">
-            <h1
-              className={`txt-heading-medium md:txt-heading-xlarge text-white lg:max-w-[480px]`}
-            >
-              {t("homepage_title")}
-            </h1>
-            <p className="txt-md-bold text-white lg:text-start md:text-xl lg:max-w-[480px]">
-              {t("homepage_subtitle")}
-            </p>
-            <div className="w-full flex flex-col justify-center gap-4 md:gap-6 md:flex-row lg:justify-start">
-              {FindPOSBtn}
-              {GetFreePOSBtn}
-            </div>
-          </HeroSection>
+      <div className="flex flex-col items-end mx-auto max-w-[1300px] lg:flex-row">
+        <Box className="flex-1 pb-6 lg:pb-[120px]">
+          <h1 className={`txt-heading-medium lg:txt-heading-large`}>
+            {HTMLReactParser(Hp2Translation.heading[locale])}
+          </h1>
+          <span className="txt-md-bold mt-2 lg:mt-6">
+            {`${Hp2Translation.update_date[locale]} ${dayjs()
+              .locale(router.locale || "en")
+              .format("MMMM YYYY")}`}
+          </span>
+          <h2 className="hidden txt-md text-neutral-700 sm:block lg:text-start">
+            {Hp2Translation.title[locale]}
+          </h2>
 
-          <div className="flex-1 relative w-full aspect-[750/700] self-end lg:max-w-[750px]">
-            <Image
-              alt="pos-home"
-              priority
-              fill
-              quality={85}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              src="https://res.cloudinary.com/dgrym3yz3/image/upload/c_scale,h_700,w_750/v1681793953/assets/common/pos_vcifjt.png"
+          <div className="flex flex-col-reverse mt-2 gap-1 lg:gap-4 lg:flex-col lg:mt-6">
+            <p className="txt-xs text-neutral-600 lg:txt-sm-bold lg:text-neutral-700">
+              {Hp2Translation.description[locale]}
+            </p>
+            <Button
+              classname="md:h-16 w-full sm:w-[266px]"
+              title={t("get_started")}
+              onClick={findPOS}
             />
           </div>
+        </Box>
+
+        <div className="hidden w-[500px] lg:inline-block">
+          <Image
+            alt="hp-2-image"
+            src="https://res.cloudinary.com/dgrym3yz3/image/upload/c_scale,h_700,w_750/v1681793953/assets/common/pos_vcifjt.png"
+            width={480}
+            height={100}
+            priority
+            className="h-auto max-w-[500px]"
+          />
         </div>
       </div>
 
-      <div
-        className="grid grid-cols-3 px-4 py-3 md:py-6 
-        md:px-8 mx-auto w-fit"
-      >
-        {FeatureData.map((item, index) => {
-          return (
-            <div
-              key={`${index}-feature`}
-              className="flex flex-col w-fit md:flex-row items-center gap-2 md:gap-3"
-            >
-              <IcCheck className="text-lg text-secondary" />
-              <p className="txt-sm-bold md:text-base text-neutral-700 text-center">
-                {index == 0 ? `24/7 ${t(item)}` : t(item)}
-              </p>
-            </div>
-          );
-        })}
-      </div>
       <BusinessCategorySection products={products} />
       <HelpingSection />
       <MetricSection titleColor="text-secondary" />
-      <TestimonialSection />
-      <ClientSection
-        body={
-          <>
-            <p className="txt-md md:text-lg lg:max-w-2xl text-neutral-700">
-              {HTMLReactParser(t("home_page_client_desc"))}
-            </p>
-            <Button
-              title={t("btn_pos_review")}
-              isOutLine={true}
-              rightIcon={<IcRightArrow className="text-lg" />}
-              onClick={() => router.push(AppRoutes.POSSystemPage)}
-            />
-          </>
-        }
-      />
-      <FooterCTA
-        bgColor="bg-accent"
-        actions={
-          <>
-            {FindPOSBtn}
-            {GetFreePOSBtn}
-          </>
-        }
-        title={t("homepage_title")}
-        des={t("homepage_subtitle")}
-      />
+      <TestimonialSectionV2 />
+      <FooterCTA />
     </>
   );
 };
