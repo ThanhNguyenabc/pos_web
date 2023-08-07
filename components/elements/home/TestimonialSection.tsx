97febfaad7a4b765d2b96bea53bd6a0ba9d21dca
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-08-07 11:55:28 +0700

    change UI for testimonials

--- components/elements/home/TestimonialSection.tsx
+++ components/elements/home/TestimonialSection.tsx
@@ -1,23 +1,33 @@
-"use client";
-
-import HeroSection from "components/common/HeroSection";
 import React from "react";
-import ReviewerSection from "../partner/ReviewerSection";
+import HeroSection from "components/common/HeroSection";
 import Loading from "components/common/loading/Loading";
-import useSWRImmutable from "swr/immutable";
+import useSWRImmutable from "swr";
 import { fetchTestimonials } from "api_client/axios_client";
+import ReviewerSectionV2 from "./ReviewerSection";
+import { Locale } from "models/app_configs";
+import useTrans from "hooks/useTrans";
 
-const TestimonialSection = () => {
+const TestimonialSectionV2Translate = {
+  heading: {
+    [Locale.en]: "See What Our Customers Are Saying",
+    [Locale.es]: "Perhaps some captcha settings",
+  },
+};
+const TestimonialSectionV2 = () => {
+  const { locale } = useTrans();
   const { data, isLoading } = useSWRImmutable(
     "testimonials",
     fetchTestimonials
   );
   return (
     <HeroSection>
+      <h3 className="txt-heading-medium md:txt-heading-large text-center lg:w-[600px] m-auto">
+        {TestimonialSectionV2Translate.heading[locale]}
+      </h3>
       {isLoading && <Loading />}
-      {data && <ReviewerSection reviews={data} />}
+      {data && <ReviewerSectionV2 reviews={data} />}
     </HeroSection>
   );
 };
 
-export default TestimonialSection;
+export default TestimonialSectionV2;
