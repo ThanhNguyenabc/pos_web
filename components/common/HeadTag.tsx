8b5ccc11845bcf8851fecf5db957d202c623e4a9
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-08-10 15:38:21 +0700

    update head tag

--- components/common/HeadTag.tsx
+++ components/common/HeadTag.tsx
@@ -32,7 +32,10 @@ const HeadTag = ({ screen, customTag }: HeadTagProps) => {
       <meta name="description" content={description} />
       <meta property={`og:title`} content={title} />
       <meta property={`og:description`} content={description} />
-
+      <meta
+        name="google-site-verification"
+        content="9Z7pkHHa-SyW7x_jI3OaN3L3OfjbQLCJWujrmVsoK70"
+      />
       <meta name="keywords" content={appConfig?.metaTags?.keywords[locale]} />
       <meta name="robots" content="INDEX,FOLLOW" />
       <meta name="msvalidate.01" content="3C6845B8D23659F8E98DDA4C3166E803" />
