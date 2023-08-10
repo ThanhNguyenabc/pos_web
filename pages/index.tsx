0f861f7a30644b17c35cd9ee74ffe6360d2d8520
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-08-10 16:18:39 +0700

    update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,20 +36,17 @@ const HomePage = ({ products }: { products: Array<Product> }) => {
   return (
     <>
       <HeadTag screen="home" />
-      <div className="flex flex-col items-end mx-auto max-w-[1300px] lg:flex-row">
-        <Box className="flex-1 pb-6 lg:pb-[120px]">
-          <h1 className={`txt-heading-medium lg:txt-heading-large`}>
-            {HTMLReactParser(t("home_page.heading"))}
-          </h1>
+      <div className="flex flex-col gap-4 items-end mx-auto max-w-[1300px] lg:gap-8 lg:flex-row">
+        <Box className="flex-1 pb-4 lg:max-w-[600px] lg:pb-8">
           <span className="txt-md-bold mt-2 lg:mt-6">
             {`${t("last_updated")} ${getCurrentMonth(locale)}`}
           </span>
-          <h2 className="hidden txt-md text-neutral-700 sm:block lg:text-start">
-            {t("home_page.sub_title")}
-          </h2>
+          <h1 className={`txt-heading-medium lg:txt-heading-large`}>
+            {HTMLReactParser(t("home_page.heading"))}
+          </h1>
 
-          <div className="flex flex-col-reverse mt-2 gap-1 lg:gap-4 lg:flex-col lg:mt-6">
-            <p className="txt-xs text-neutral-600 lg:txt-sm-bold lg:text-neutral-700">
+          <div className="flex flex-col mt-2 gap-4 lg:mt-4">
+            <p className="txt-sm text-neutral-600 lg:text-base lg:text-neutral-700">
               {t("home_page.desc")}
             </p>
             <Button
@@ -60,15 +57,15 @@ const HomePage = ({ products }: { products: Array<Product> }) => {
           </div>
         </Box>
 
-        <div className="hidden w-[500px] lg:inline-block">
+        <div className="hidden max-w-[580px] lg:inline-block">
           <Image
             alt="hp-2-image"
-            src="https://res.cloudinary.com/dgrym3yz3/image/upload/c_scale,h_700,w_750/v1681793953/assets/common/pos_vcifjt.png"
-            width={750}
-            height={700}
+            src = "https://res.cloudinary.com/dgrym3yz3/image/upload/v1691658186/assets/common/pos_cover_i2ynqk.png"
+            width={1170}
+            height={640}
             priority
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="h-auto max-w-[500px]"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 40vw"
+            className="h-auto max-w-[580px]"
           />
         </div>
       </div>
