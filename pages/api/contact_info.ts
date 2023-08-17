4975ace0cc8f6d3dedfb9471be4bbdc47c1aa955
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-08-17 16:48:34 +0700

    print log

--- pages/api/contact_info.ts
+++ pages/api/contact_info.ts
@@ -9,40 +9,45 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  switch (req.method) {
-    case "POST":
-      const { conversion_funnel, ref_url, data } = req.body as DataSubmission;
-      const { phone, email, name, zipcode, message } = data as ContactInfo;
-      let content = [
-        `Customer name: ${name}`,
-        `Customer email: ${email}`,
-        `Customer phone number: ${phone}`,
-        `Zipcode: ${zipcode}`,
-        `Message: ${message}`,
-      ];
+  try {
+    switch (req.method) {
+      case "POST":
+        const { conversion_funnel, ref_url, data } = req.body as DataSubmission;
+        const { phone, email, name, zipcode, message } = data as ContactInfo;
+        let content = [
+          `Customer name: ${name}`,
+          `Customer email: ${email}`,
+          `Customer phone number: ${phone}`,
+          `Zipcode: ${zipcode}`,
+          `Message: ${message}`,
+        ];
 
-      const emailContent = await getEmailTemplate(name);
+        const emailContent = await getEmailTemplate(name);
 
-      const promises = [
-        insertDataToGooglesheet({
-          conversion_funnel,
-          ref_url,
-          data: content.join("\n"),
-          customer_name: name,
-          customer_phone: phone,
-        }),
-        sendEmail({
-          subject: "Bestpos lead - Contact",
-          html: `<b>We have new customer with the following information</b><br>
+        const promises = [
+          insertDataToGooglesheet({
+            conversion_funnel,
+            ref_url,
+            data: content.join("\n"),
+            customer_name: name,
+            customer_phone: phone,
+          }),
+          sendEmail({
+            subject: "Bestpos lead - Contact",
+            html: `<b>We have new customer with the following information</b><br>
           ${content.join("<br>")}`,
-        }),
-        sendEmail({
-          subject: "Bestpos.com",
-          html: emailContent,
-          to: email,
-        }),
-      ];
-      Promise.all(promises);
-      return res.status(200).json({ data: true });
+          }),
+          sendEmail({
+            subject: "Bestpos.com",
+            html: emailContent,
+            to: email,
+          }),
+        ];
+        Promise.all(promises);
+        return res.status(200).json({ data: true });
+    }
+  } catch (error) {
+    console.log("error------contact");
+    console.log(error);
   }
 }
