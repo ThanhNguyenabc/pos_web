55d7270ab299cc96a3cd26479a9aa7712a920a79
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-08-17 16:19:31 +0700

    update patth file

--- lib/template/index.ts
+++ lib/template/index.ts
@@ -1,12 +1,13 @@
 import { readFile } from "fs/promises";
 import ejs from "ejs";
+import path from "path";
 
 var cacheTemplate: string = "";
 export const getEmailTemplate = async (name: string = "") => {
   let template = cacheTemplate;
   if (template.length == 0) {
     const temp = await readFile(
-      `${process.cwd()}/public/request_demo_email_template.html`,
+      path.resolve("public/request_demo_email_template.html"),
       "utf-8"
     );
     template = cacheTemplate = temp;
