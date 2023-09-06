5a12adb6361f67326476165ad67347e18617ed8f
🚀thanhnguyen <thanhnguyen95abc@gmail.com>
2023-09-06 16:23:25 +0700

    remove unused code

--- assets/AssetUtil.tsx
+++ assets/AssetUtil.tsx
@@ -2,18 +2,6 @@
 import DefaultImg from "./images/default-image.webp";
 import Logo from "./images/pos_logo.png";
 
-// const BusinessImg =
-//   "https://res.cloudinary.com/dgrym3yz3/image/upload/v1681793947/assets/common/business_img_jqrnqm.png";
-// const QuickServiceImg =
-//   "https://res.cloudinary.com/dgrym3yz3/image/upload/v1681793948/assets/common/quickservice_img_kk8p5d.png";
-// const RetailImg =
-//   "https://res.cloudinary.com/dgrym3yz3/image/upload/v1681793949/assets/common/retail_img_nnwh2y.png";
-// const PizzeriaImg =
-//   "https://res.cloudinary.com/dgrym3yz3/image/upload/v1681793948/assets/common/pizzeria_img_tqeeij.png";
-// const RestaurantImg =
-//   "https://res.cloudinary.com/dgrym3yz3/image/upload/v1681793949/assets/common/restaurant_img_sutyep.png";
-// const BarImg =
-//   "https://res.cloudinary.com/dgrym3yz3/image/upload/v1681793947/assets/common/bar_img_mx05pk.png";
 const Station1Img =
   "https://res.cloudinary.com/dgrym3yz3/image/upload/v1681793951/assets/common/station_1_sdk9k5.png";
 const Station2Img =
